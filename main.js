8de81aa6d07b09cbf9315fc8ab08ab32c9fdc6c0
Armin Barzegar Amin <armin.barzegaramin@student.ap.be>
2023-03-11 17:09:40 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+
+import './reset.css'
+import './style.css'
+
+
+  // import Swiper JS
+import Swiper from 'swiper';
+  // import Swiper styles
+import 'swiper/css';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+async function reverseGeocode(lat, lng) {
+  try {
+    const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?types=address&access_token=pk.eyJ1IjoiYXJtaW5kcyIsImEiOiJjbGVta3F5OGIxNm9yM3ZtaXhrbDVzOTR6In0.CKcXAK61aJH7wOTgmULKpQ&language=en&limit=1`);
+    const data = await response.json();
+    return data.features[0].place_name;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function performReverseGeocoding() {
+  const lat = 51.277140;
+  const lng = 4.418260;
+
+  try {
+
+    // Show the loader
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'block';
+
+    const placeName = await reverseGeocode(lat, lng);
+    console.log(placeName)
+    document.getElementById("footer-left-reversegeolocation").textContent = placeName
+
+    // Hide the loader
+    loader.style.display = 'none';
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+performReverseGeocoding();
+
